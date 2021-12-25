6b3c5f46e4bdf8d4372e825803bb8c56a6539ac8
BianWenKai <1467295546@qq.com>
2021-12-26 00:09:39 +0800

    JS异步：Promise、Async、Await

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,23 +1,49 @@
 console.log('before');
-getUser(1, (user) => {
-    getRespositories(user.gitHubName, (repos) => {
+
+//当调用较多时，callback 地狱
+// getUser(1, (user) => {
+//     getRespositories(user.gitHubName, (repos) => {
+//          console.log(repos));
+//      }
+// })
+
+//使用 Promises 实现异步编程
+getUser(1)
+    .then(user => getRespositories(user.gitHubName))
+    .then(repos => console.log(repos))
+    .catch(err => console.log("Error:", err.message));
+
+
+// Async and Await
+async function displayRepos() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRespositories(user.gitHubName);
         console.log(repos);
-    })
-})
+    } catch (err) {
+        console.log('Error:', err.message);
+    }
+}
+displayRepos();
+
 console.log('after');
 
 
 
-function getUser(id, callback) {
-    setTimeout(() => {
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
         console.log('get user from database');
-        callback({id : id, gitHubName : 'kevin'});
-    })
+        resolve({id : id, gitHubName : 'kevin'});
+    }, 2000);
+    });
 }
 
-function getRespositories(user, callback) {
-    setTimeout( () => {
-        let repos = ['respos1', 'respos2'];
-        callback(`${user} 的仓库 : ${repos} `);
-    }, 2000);
+function getRespositories(user) {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            let repos = ['respos1', 'respos2'];
+            resolve(`${user} 的仓库 : ${repos} `);
+        }, 2000);
+    });
 }

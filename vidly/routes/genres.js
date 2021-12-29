6d039c8e8a9c3eca7a2f508aa2ebcf22e5b3cf19
BianWenKai <1467295546@qq.com>
2021-12-29 18:37:02 +0800

    使用中间函数auth，校验用户token，保护路由

--- vidly/routes/genres.js
+++ vidly/routes/genres.js
@@ -2,24 +2,27 @@ const {Genre, validate} = require('../models/genre');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.post('/', async (req, res) => {
-  const { error } = validate(req.body); 
+//auth:middleware function. 验证当前用户,非注册用户无法使用该功能
+router.post('/', auth, async (req, res) => {
+
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
-  
+
   res.send(genre);
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validate(req.body); 
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
@@ -27,7 +30,7 @@ router.put('/:id', async (req, res) => {
   });
 
   if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-  
+
   res.send(genre);
 });
 
